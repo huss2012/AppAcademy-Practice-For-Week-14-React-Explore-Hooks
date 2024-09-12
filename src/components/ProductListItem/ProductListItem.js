0c180565dd8615506692fdba032a59111eaba93f
huss2012 <huss24940@gmail.com>
2024-09-12 08:51:22 +0300

    Phase 4 is done.

--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -4,8 +4,8 @@ function ProductListItem({ product, isSelected, onClick }) {
   return (
     <div className={`product-list-item ${isSelected ? ' selected' : ''}`}>
       <img className="product-list-item-photo"
-           src={product.photo.filename}
-           alt={`${product.name}`}
+        src={product.photo.filename}
+        alt={`${product.name}`}
       />
       <button onClick={onClick}>{product.name}</button>
     </div>
--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -5,16 +5,36 @@ import './ProductView.css'
 
 function ProductView({ products }) {
 
-  const [sideOpen, setSideOpen] = useState(true);
-  const [selectedProduct, setSelectedProduct] = useState();
+  const [sideOpen, setSideOpen] = useState(() => {
+    const savedState = localStorage.getItem("sideOpen");
+    return savedState !== null ? JSON.parse(savedState) : true
+  });
+  const [selectedProduct, setSelectedProduct] = useState(() => {
+    const savedProductId = localStorage.getItem("selectedProduct");
+    return savedProductId ? products.find(product => product.id === JSON.parse(savedProductId)): null
+  });
 
   useEffect(() => {
-    setSideOpen(true);
+    if (selectedProduct) setSideOpen(true);
   }, [selectedProduct]);
 
   useEffect(() => {
-    setSelectedProduct();
+    if (!sideOpen) setSelectedProduct(null);
   }, [sideOpen]);
+
+  useEffect(() => {
+    console.log("UseEffect responsople for localStorage is running")
+    localStorage.setItem("sideOpen", JSON.stringify(sideOpen));
+  }, [sideOpen]);
+
+  useEffect(() => {
+    if (selectedProduct) {
+      localStorage.setItem('selectedProduct', selectedProduct.id)
+    } else {
+      localStorage.removeItem('selectedProduct')
+    }
+  }, [selectedProduct]);
+
   return (
     <div className="product-view">
       <div className="product-main-area">
